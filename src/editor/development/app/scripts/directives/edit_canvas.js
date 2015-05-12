c06f63862c4679b16ca8e8a9da8f00d895899992
Claudio Fernandes <claudiosf.claudio@gmail.com>
2015-05-11 22:42:45 -0300

    First editor camera.

--- src/editor/development/app/scripts/directives/edit_canvas.js
+++ src/editor/development/app/scripts/directives/edit_canvas.js
@@ -40,6 +40,74 @@ angular.module('lauEditor')
     }
   }
 
+  function initHorizontalGrid() {
+      planeMesh = new THREE.GridHelper(1000.0, 100);
+      planeMesh.setColors(0x4a4a4a, 0x4a4a4a);
+      scene.add( planeMesh );
+  }
+  function initCamera() {
+    // TODO: Handle scroll (maybe change pivot and sensitivities)
+    var pivot_distance = 1000.00;
+    var translate_sensitivity = 3.0;
+
+    function translateCamera(moveE) {
+      var cameraQuat = camera.getWorldQuaternion();
+      var X = new THREE.Vector3(1,0,0).applyQuaternion(cameraQuat);
+      var Y = new THREE.Vector3(0,1,0).applyQuaternion(cameraQuat);
+
+      X.multiplyScalar(-moveE.movementX * translate_sensitivity);
+      Y.multiplyScalar(moveE.movementY * translate_sensitivity);
+
+      camera.position.add(X.add(Y));
+    }
+
+    function rotateCamera(moveE) {
+      var focalLength = 10.0;
+
+      var pivot = new THREE.Vector3(0,0,-pivot_distance);
+      var rotationAxis = new THREE.Vector3().crossVectors(
+        new THREE.Vector3(0,0,-focalLength).normalize(),
+        new THREE.Vector3(moveE.movementX, -moveE.movementY, -focalLength).normalize()
+      ).normalize();
+      var movement = Math.sqrt(moveE.movementY*moveE.movementY +
+                               moveE.movementX*moveE.movementX);
+      var rotationAngle = Math.atan2(movement, 100.0);
+      var rotation = new THREE.Quaternion().setFromAxisAngle(
+        rotationAxis, rotationAngle
+      );
+
+      // Rotate/translate camera
+      var origPivot = pivot.clone().applyQuaternion(camera.quaternion);
+      camera.quaternion.multiply(rotation);
+      var rotatedPivot = pivot.clone().applyQuaternion(camera.quaternion);
+      camera.position.add(origPivot).sub(rotatedPivot);
+    }
+
+    // Prevent context menu
+    $(renderer.domElement).on('contextmenu', function(e){return false;});
+    // Setup mousedown
+    renderer.domElement.onmousedown = function(downE) {
+      var eventHandler = null;
+      switch(downE.button) {
+        case 0: // Left
+          return;
+        case 1: // Middle
+          eventHandler = translateCamera;
+        break;
+        case 2: // Right
+          eventHandler = rotateCamera;
+        break;
+      }
+
+      $canvas.requestPointerLock();
+      renderer.domElement.addEventListener('mousemove', eventHandler);
+      renderer.domElement.addEventListener('mouseup', function() {
+        renderer.domElement.removeEventListener('mousemove', eventHandler);
+        document.exitPointerLock();
+      });
+    };
+  }
+
   function initCanvas(containerElement) {
       scene = new THREE.Scene();
 
@@ -49,10 +117,12 @@ angular.module('lauEditor')
 
       renderer = new THREE.WebGLRenderer();
       renderer.setSize( width, height );
+      renderer.setClearColor(0x393939, 1.0);
       containerElement.append(renderer.domElement);
-      planeMesh = new THREE.GridHelper(1000.0, 100);
-      scene.add( planeMesh );
       $canvas = renderer.domElement;
+
+      initHorizontalGrid();
+      initCamera();
   }
 
   return {
