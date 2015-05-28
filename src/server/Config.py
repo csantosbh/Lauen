b7395b5832a645e2bf60eae6b52de854720da950
Claudio Fernandes <claudiosf.claudio@gmail.com>
2015-05-27 22:59:55 -0300

    lau-31: Saving list of last created projects.
    Now the settings are stored inside ~/.laurc, which is a JSON file.

--- src/server/Config.py
+++ src/server/Config.py
@@ -1,7 +1,49 @@
 import ConfigParser, os
+import json
+import collections
 
-config = ConfigParser.ConfigParser()
-config.read('../config/config.cfg')
+defaults=dict(
+  DEFAULT=dict(
+    port='8080',
+    recent_project_history=5,
+    recent_projects=[],
+  )
+)
+
+config = defaults
+
+def _update(d, u):
+    for k, v in u.iteritems():
+        if isinstance(v, collections.Mapping):
+            r = _update(d.get(k, {}), v)
+            d[k] = r
+        else:
+            d[k] = u[k]
+    return d
+
+def _initialize():
+    user_config = json.loads(open(os.path.expanduser('~/.laurc'), 'r').read())
+    config = _update(defaults, user_config)
+    open(os.path.expanduser('~/.laurc'), 'w').write(json.dumps(config, indent=2))
+    pass
+
+def _sanitizeRequest(section, field):
+    if not config.has_key(section):
+        config[section] = dict()
+        pass
+    if not config[section].has_key(section):
+        config[section][field] = config['DEFAULT'][field]
+        pass
+    pass
 
 def get(section, field):
-    return config.get(section, field)
+    _sanitizeRequest(section, field)
+    return config[section][field]
+
+def set(section, field, value):
+    _sanitizeRequest(section, field)
+    config[section][field] = value
+    open(os.path.expanduser('~/.laurc'), 'w').write(json.dumps(config, indent=2))
+    pass
+
+_initialize()
--- src/server/project/ProjectEventHandler.py
+++ src/server/project/ProjectEventHandler.py
@@ -1,7 +1,8 @@
 from server import RPC
+from server import Config
 
 def getRecentProjects(evData):
-    return ['/home/csantos/workspace/LauEngine/']
+    return Config.get('runtime', 'recent_projects')
 
 def createNewProject(evData):
     from Tkinter import Tk
@@ -12,6 +13,17 @@ def createNewProject(evData):
     root.style.theme_use('clam')
     root.withdraw()
     in_path = tkFileDialog.askdirectory()
+
+    if len(in_path)!=0:
+        # Create new project!
+        recent_projects = Config.get('runtime', 'recent_projects')
+        recent_projects.insert(0, in_path)
+        if len(recent_projects) > Config.get('project', 'recent_project_history'):
+            recent_projects.pop(-1)
+            pass
+        Config.set('runtime', 'recent_projects', recent_projects)
+        pass
+
     return in_path
 
 RPC.listen(createNewProject)
