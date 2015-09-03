bae0ffa409b73ef435956ba7885a9bd7a31072fb
Claudio Fernandes <claudiosf.claudio@gmail.com>
2015-09-02 21:24:48 -0300

    Refactoring drag&drop system.

---
+++ src/editor/development/app/scripts/services/dragdrop_manager.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name lauEditor.dragdropManager
+ * @description
+ * # dragdropManager
+ * Service in the lauEditor.
+ */
+angular.module('lauEditor')
+  .service('dragdropManager', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
